3c9a52c00ab926dac964ab6740802e7d432fee88
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-06 17:10:10 -0300

    modificando nome e status code da exceção

--- BookStore/src/main/java/br/com/books/exceptions/ResourceNotFoundException.java
+++ BookStore/src/main/java/br/com/books/exceptions/ResourceNotFoundException.java
@@ -3,12 +3,12 @@ package br.com.books.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException{
-	public UnsupportedMathOperationException(String msg) {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
-
-	private static final long serialVersionUID = 1L;
 
 }

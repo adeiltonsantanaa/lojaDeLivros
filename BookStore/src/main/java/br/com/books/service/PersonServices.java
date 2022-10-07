58f895013ac3fb57c7aea23364c3845298511dee
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-07 18:56:03 -0300

    implementando o HATEOAS nos EndPoints

--- BookStore/src/main/java/br/com/books/service/PersonServices.java
+++ BookStore/src/main/java/br/com/books/service/PersonServices.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import br.com.books.controllers.PersonController;
 import br.com.books.data.vo.v1.PersonVO;
+import br.com.books.exceptions.RequiredObjectNullException;
 import br.com.books.exceptions.ResourceNotFoundException;
 import br.com.books.mapper.DozerMapper;
 import br.com.books.model.Person;
@@ -20,16 +24,26 @@ public class PersonServices {
 	public PersonVO findById(Long id) {
 		var person = personRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Pessoa não encontrada. " + id));
-		return DozerMapper.parseObject(person, PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(person, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findAll()).withSelfRel());
+		return vo;
 	}
 
 	public List<PersonVO> findAll() {
-		return DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+		var persons = DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+		persons.stream()
+				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
 	}
 
 	public PersonVO create(PersonVO person) {
+		if (person == null) {
+			throw new RequiredObjectNullException("Não é possível persistir uma pessoa nula!");
+		}
 		var personSave = DozerMapper.parseObject(person, Person.class);
-		return DozerMapper.parseObject(personRepository.save(personSave), PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(personRepository.save(personSave), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
 	}
 
 	public PersonVO update(PersonVO personVO) {
@@ -40,7 +54,9 @@ public class PersonServices {
 		person2.setLastName(person.getLastName());
 		person2.setAddress(person.getAddress());
 		person2.setGender(person.getGender());
-		return DozerMapper.parseObject(personRepository.save(person2), PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(personRepository.save(person2), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
 	}
 
 	public void delete(Long id) {

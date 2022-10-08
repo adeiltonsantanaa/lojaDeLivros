e860d31659cdf9e6923b5db03416f240c47bf9b2
Adeilton Santana <filhoaguiar20@gmail.com>
2022-10-08 00:41:07 -0300

    criação do mock e testes unitários de book

---
+++ BookStore/src/test/java/br/com/books/service/BookServicesTest.java
@@ -0,0 +1,133 @@
+package br.com.books.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.books.data.vo.v1.BookVO;
+import br.com.books.model.Book;
+import br.com.books.repositories.BookRepository;
+import br.com.books.unittests.mapper.mock.MockBook;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class BookServicesTest {
+
+	MockBook input;
+
+	@InjectMocks
+	private BookServices service;
+
+	@Mock
+	BookRepository repository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		input = new MockBook();
+		MockitoAnnotations.openMocks(input);
+	}
+
+	@Test
+	void testFindById() {
+		Book book = input.mockEntity(1);
+		book.setId(1L);
+
+		when(repository.findById(1L)).thenReturn(Optional.of(book));
+
+		var result = service.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		System.out.println(result.toString());
+		assertTrue(result.toString().contains("links: [</api/v1/book>;rel=\"self\"]"));
+		assertEquals("Author Test1", result.getAuthor());
+		equals(LocalDate.now());
+		assertEquals("Title Test1", result.getTitle());
+
+	}
+
+	@Test
+	void testFindAll() {
+		List<Book> list = input.mockEntityList();
+
+		when(repository.findAll()).thenReturn(list);
+
+		var books = service.findAll();
+		assertNotNull(books);
+		assertEquals(14, books.size());
+
+		var bookOne = books.get(1);
+		assertNotNull(bookOne);
+		assertNotNull(bookOne.getKey());
+		assertNotNull(bookOne.getLinks());
+
+		System.out.println(bookOne.toString());
+		assertTrue(bookOne.toString().contains("links: [</api/v1/book/1>;rel=\"self\"]"));
+		assertEquals("Author Test1", bookOne.getAuthor());
+		equals(LocalDate.now());
+		assertEquals("Title Test1", bookOne.getTitle());
+
+		var bookTwo = books.get(2);
+		assertNotNull(bookTwo);
+		assertNotNull(bookTwo.getKey());
+		assertNotNull(bookTwo.getLinks());
+
+		System.out.println(bookTwo.toString());
+		assertTrue(bookTwo.toString().contains("links: [</api/v1/book/2>;rel=\"self\"]"));
+		assertEquals("Author Test2", bookTwo.getAuthor());
+		equals(LocalDate.now());
+		assertEquals("Title Test2", bookTwo.getTitle());
+
+	}
+
+	@Test
+	void testCreate() {
+		Book book = input.mockEntity(1);
+		Book persisted = book;
+		persisted.setId(1L);
+		
+		BookVO vo = input.mockVO(1);
+		vo.setKey(1L);
+
+		when(repository.save(book)).thenReturn(persisted);
+		var result = service.create(vo);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		System.out.println(result.toString());
+		assertTrue(result.toString().contains("links: [</api/v1/book>;rel=\"self\"]"));
+		assertEquals("Author Test1", result.getAuthor());
+		equals(LocalDate.now());
+		assertEquals("Title Test1", result.getTitle());
+		
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BookStore/src/test/java/br/com/books/unittests/mapper/mock/MockBook.java
@@ -0,0 +1,56 @@
+package br.com.books.unittests.mapper.mock;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.books.data.vo.v1.BookVO;
+import br.com.books.model.Book;
+
+public class MockBook {
+
+	public Book mockEntity() {
+		return mockEntity(0);
+	}
+
+	public BookVO mockVO() {
+		return mockVO(0);
+	}
+
+	public List<Book> mockEntityList() {
+		List<Book> books = new ArrayList<Book>();
+		for (int i = 0; i < 14; i++) {
+			books.add(mockEntity(i));
+		}
+		return books;
+	}
+
+	public List<BookVO> mockVOList() {
+		List<BookVO> books = new ArrayList<>();
+		for (int i = 0; i < 14; i++) {
+			books.add(mockVO(i));
+		}
+		return books;
+	}
+
+	public Book mockEntity(Integer number) {
+		Book book = new Book();
+		book.setAuthor("Author Test" + number);
+		book.setLaunchDate(LocalDate.now());
+		book.setPrice(20.0F);
+		book.setId(number.longValue());
+		book.setTitle("Title Test" + number);
+		return book;
+	}
+
+	public BookVO mockVO(Integer number) {
+		BookVO bookVO = new BookVO();
+		bookVO.setAuthor("Author Test" + number);
+		bookVO.setLaunchDate(LocalDate.now());
+		bookVO.setPrice(20.0F);
+		bookVO.setKey(number.longValue());
+		bookVO.setTitle("Last Name Test" + number);
+		return bookVO;
+	}
+
+}
